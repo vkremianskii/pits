b5aa3e2e9b953e3bce3e341fdd96097f7d646f67
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-05-24 07:10:39 +0700

    [communicator] Log incoming gRPC calls

--- communicator/communicator-app/src/main/java/com/github/vkremianskii/pits/communicator/app/grpc/GrpcServer.java
+++ communicator/communicator-app/src/main/java/com/github/vkremianskii/pits/communicator/app/grpc/GrpcServer.java
@@ -1,11 +1,15 @@
 package com.github.vkremianskii.pits.communicator.app.grpc;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import com.github.vkremianskii.pits.communicator.grpc.EquipmentServiceGrpc;
+import io.grpc.*;
+import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -28,6 +32,7 @@ public class GrpcServer {
     @EventListener
     public void handleContextStart(ContextRefreshedEvent event) throws IOException {
         server = ServerBuilder.forPort(grpcProperties.getPort())
+                .intercept(new RequestLoggingInterceptor())
                 .addService(new EquipmentServiceImpl(rabbitTemplate))
                 .build();
 
@@ -38,5 +43,21 @@ public class GrpcServer {
     public void handleContextStop(ContextStoppedEvent event) throws InterruptedException {
         server.shutdown();
         server.awaitTermination();
+    }
+
+    private static class RequestLoggingInterceptor implements ServerInterceptor {
+        private static final Logger LOG = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+        @Override
+        public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+            final var listener = next.startCall(call, headers);
+            return new SimpleForwardingServerCallListener<>(listener) {
+                @Override
+                public void onMessage(ReqT message) {
+                    LOG.info("Call: " + System.lineSeparator() + message);
+                    super.onMessage(message);
+                }
+            };
+        }
     }
 }
