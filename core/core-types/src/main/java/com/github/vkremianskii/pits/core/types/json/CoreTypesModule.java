b737050b7bb9c69098ce381c90d548597058b2ac
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-05-29 13:10:32 +0700

    Remove redundant MicrotypeDeserializer
    Jackson relies on presence of `valueOf`.

--- core/core-types/src/main/java/com/github/vkremianskii/pits/core/types/json/CoreTypesModule.java
+++ core/core-types/src/main/java/com/github/vkremianskii/pits/core/types/json/CoreTypesModule.java
@@ -6,13 +6,10 @@ import com.fasterxml.jackson.databind.module.SimpleSerializers;
 import com.github.vkremianskii.pits.core.types.json.deserializer.EquipmentDeserializer;
 import com.github.vkremianskii.pits.core.types.json.deserializer.EquipmentStateDeserializer;
 import com.github.vkremianskii.pits.core.types.json.deserializer.LocationDeserializer;
-import com.github.vkremianskii.pits.core.types.json.deserializer.MicrotypeDeserializer;
 import com.github.vkremianskii.pits.core.types.json.serializer.EquipmentStateSerializer;
 import com.github.vkremianskii.pits.core.types.json.serializer.MicrotypeSerializer;
 import com.github.vkremianskii.pits.core.types.model.Equipment;
-import com.github.vkremianskii.pits.core.types.model.EquipmentId;
 import com.github.vkremianskii.pits.core.types.model.Location;
-import com.github.vkremianskii.pits.core.types.model.LocationId;
 import com.github.vkremianskii.pits.core.types.model.equipment.DozerState;
 import com.github.vkremianskii.pits.core.types.model.equipment.DrillState;
 import com.github.vkremianskii.pits.core.types.model.equipment.ShovelState;
@@ -20,7 +17,6 @@ import com.github.vkremianskii.pits.core.types.model.equipment.TruckState;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import static com.github.vkremianskii.pits.core.types.model.EquipmentType.DOZER;
 import static com.github.vkremianskii.pits.core.types.model.EquipmentType.DRILL;
@@ -36,13 +32,11 @@ public class CoreTypesModule extends SimpleModule {
             new EquipmentStateSerializer()
         )));
         context.addDeserializers(new SimpleDeserializers(Map.of(
-            EquipmentId.class, new MicrotypeDeserializer<>(EquipmentId.class, UUID.class),
             Equipment.class, new EquipmentDeserializer(),
             DozerState.class, new EquipmentStateDeserializer(DOZER),
             DrillState.class, new EquipmentStateDeserializer(DRILL),
             ShovelState.class, new EquipmentStateDeserializer(SHOVEL),
             TruckState.class, new EquipmentStateDeserializer(TRUCK),
-            LocationId.class, new MicrotypeDeserializer<>(LocationId.class, UUID.class),
             Location.class, new LocationDeserializer()
         )));
     }
