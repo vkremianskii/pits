66293e3f714569a526f5c1ac2dc74c9da7bb7e47
Vsevolod Kremianskii <vkremianskii@gmail.com>
2022-06-01 21:17:22 +0700

    Fix AMQP message serialization

---
+++ communicator/communicator-app/src/main/java/com/github/vkremianskii/pits/communicator/infra/JsonConfig.java
@@ -0,0 +1,20 @@
+package com.github.vkremianskii.pits.communicator.infra;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.vkremianskii.pits.core.json.CoreModule;
+import com.github.vkremianskii.pits.registry.json.RegistryModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    Jackson2ObjectMapperBuilderCustomizer objectMapperBuilderCustomizer() {
+        return objectMapperBuilder -> objectMapperBuilder
+            .modules(new CoreModule(), new RegistryModule())
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .build();
+    }
+}
